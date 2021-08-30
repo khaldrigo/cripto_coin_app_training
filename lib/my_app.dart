4aa0bfd7a1a464ff33d24e606e87fc2d0ad0dd2c
Rodrigo <rodrigolimadh@gmail.com>
2021-08-30 11:25:12 -0300

    atualizando componentes

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BaseCoins',
+      title: 'MoedasBase',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
