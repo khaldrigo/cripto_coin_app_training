8009818dd7785caaca1f91bdce5d312fd9a73bde
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-08-30 16:49:35 -0300

    formularios, campos de texto, tela de compras e reatividade

--- lib/pages/moedas_detalhes_page.dart
+++ lib/pages/moedas_detalhes_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cripto_coin/models/moedas.dart';
+import 'package:intl/intl.dart';
 
 class MoedasDetalhesPage extends StatefulWidget {
   Moeda moeda;
@@ -11,6 +13,23 @@ class MoedasDetalhesPage extends StatefulWidget {
 }
 
 class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final _form = GlobalKey<FormState>();
+  final _valor = TextEditingController();
+  double quantidade = 0;
+
+  comprar() {
+    if (_form.currentState!.validate()) {
+      // Salvar a compra
+
+      Navigator.pop(context);
+      // ignore: prefer_const_constructors
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Compra realizada com sucesso!'),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,17 +39,127 @@ class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
       body: Column(
         children: [
           const Divider(),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: SizedBox(
+          Padding(
+            padding: const EdgeInsets.only(bottom: 24.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
                   width: 50,
                   child: Image.asset(widget.moeda.icone),
                 ),
+                Container(
+                  width: 10,
+                ),
+                Text(
+                  real.format(
+                    widget.moeda.preco,
+                  ),
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -1,
+                      color: Colors.grey[800]),
+                ),
+              ],
+            ),
+          ),
+          (quantidade > 0)
+              ? SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  // ignore: avoid_unnecessary_containers
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Container(
+                      child: Text(
+                        '${quantidade.toStringAsFixed(2)} ${widget.moeda.sigla}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(bottom: 24),
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.teal.withOpacity(0.05),
+                      ),
+                    ),
+                  ),
+                )
+              : Container(
+                  margin: const EdgeInsets.only(bottom: 24),
+                ),
+          Form(
+            key: _form,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextFormField(
+                controller: _valor,
+                style: const TextStyle(
+                  fontSize: 22,
+                ),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Valor',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  suffix: Text(
+                    'reais',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Informe o valor da compra!';
+                  } else if (double.parse(value) < 50) {
+                    return 'Compra mínima é de R\$50,00';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    quantidade = (value.isEmpty)
+                        ? 0
+                        : double.parse(value) / widget.moeda.preco;
+                  });
+                },
               ),
-            ],
-          )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              margin: const EdgeInsets.only(top: 24),
+              child: ElevatedButton(
+                onPressed: comprar,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Icon(Icons.check),
+                    // ignore: prefer_const_constructors
+                    const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'Comprar!',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
