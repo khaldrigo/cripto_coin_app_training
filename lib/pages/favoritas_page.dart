9f98d550db81a5c78b73131d9fc087b97066c22b
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-08-31 12:27:58 -0300

    add tela de favoritos funcional e widget MoedaCard/ remoção de favoritos

--- lib/pages/favoritas_page.dart
+++ lib/pages/favoritas_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cripto_coin/repositories/favoritas_repository.dart';
+import 'package:flutter_cripto_coin/widgets/moeda_card.dart';
+import 'package:provider/provider.dart';
 
 class FavoritasPage extends StatefulWidget {
   const FavoritasPage({Key? key}) : super(key: key);
@@ -13,6 +16,26 @@ class _FavoritasPageState extends State<FavoritasPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Moedas Favoritas'),
+      ),
+      body: Container(
+        color: Colors.indigo.withOpacity(0.05),
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(12),
+        child: Consumer<FavoritasRepository>(
+          builder: (context, favoritas, child) {
+            return favoritas.lista.isEmpty
+                ? const ListTile(
+                    leading: Icon(Icons.star),
+                    title: Text('Ainda não há moedas favoritas!'),
+                  )
+                : ListView.builder(
+                    itemBuilder: (_, index) {
+                      return MoedaCard(moeda: favoritas.lista[index]);
+                    },
+                    itemCount: favoritas.lista.length,
+                  );
+          },
+        ),
       ),
     );
   }
